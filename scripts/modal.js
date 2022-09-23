80d2f0831b0a0cc455d706bf97fe543a3f6552d1
jessica <jessica_02_06@outlook.com>
2022-09-23 12:13:12 -0300

    alteracao unidade de medida

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,1 +1,8 @@
-/* Desenvolva seu código aqui... */
+const buttonModal = document.querySelectorAll("[data_modal]")
+
+for(let i = 0; i < buttonModal.length; i++){
+    buttonModal[i].addEventListener("click", function(){
+        let valueButtonModal = buttonModal[i].getAttribute("data_modal")
+        document.getElementById(valueButtonModal).classList.toggle("show_screen")
+    })
+}
